d34a8570d2765deae6ea3fb8edd0e45bdab27188
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-02 18:15:44 -0300

    Autorização das rotas

--- start/routes.js
+++ start/routes.js
@@ -10,6 +10,8 @@ Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
 Route.group(() => {
+  Route.get('roles', 'RoleController.index')
+
   Route.resource('teams', 'TeamController')
     .apiOnly()
     .validator(new Map([[['teams.store', 'teams.update'], ['Team']]]))
@@ -18,9 +20,16 @@ Route.group(() => {
 }).middleware('auth')
 
 Route.group(() => {
-  Route.post('invites', 'InviteController.store').validator('Invite')
+  Route.post('invites', 'InviteController.store')
+    .validator('Invite')
+    .middleware('can:invites_create')
 
   Route.resource('projects', 'ProjectController')
     .apiOnly()
     .validator(new Map([[['projects.store', 'projects.update'], ['Project']]]))
+    .middleware(
+      new Map([
+        [['projects.store', 'projects.update'], ['can:projects_create']]
+      ])
+    )
 }).middleware(['auth', 'team'])
