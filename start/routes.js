743b582ebafa5380a00479b0f59342f6dd3b6aaa
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-02 18:44:34 -0300

    Permissões

--- start/routes.js
+++ start/routes.js
@@ -36,4 +36,6 @@ Route.group(() => {
   Route.put('members/:id', 'MemberController.update').middleware(
     'is:administrator'
   )
+
+  Route.get('permissions', 'PermissionController.show')
 }).middleware(['auth', 'team'])
