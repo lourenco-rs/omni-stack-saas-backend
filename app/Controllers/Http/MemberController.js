a8994e9276a0c5179d8e0bb7031f67d14a05f51a
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-02 18:35:46 -0300

    Listagem e edição de membros

---
+++ app/Controllers/Http/MemberController.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const UserTeam = use('App/Models/UserTeam')
+
+class MemberController {
+  async index ({ request }) {
+    const members = await UserTeam.query()
+      .where('team_id', request.team.id)
+      .with('user')
+      .with('roles')
+      .fetch()
+
+    return members
+  }
+
+  async update ({ request, params }) {
+    const roles = request.input('roles')
+
+    const teamJoin = await UserTeam.find(params.id)
+
+    /**
+     * sync equivale a um detach e attach
+     */
+    await teamJoin.roles().sync(roles)
+  }
+}
+
+module.exports = MemberController
