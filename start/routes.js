d75de6556eba29b835fbb520308212685369b5ea
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-02 05:10:48 -0300

    CRUD de times

--- start/routes.js
+++ start/routes.js
@@ -2,23 +2,13 @@
 
 const Route = use('Route')
 
-Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store').validator('Session')
+Route.post('users', 'UserController.store').validator('User')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
 Route.group(() => {
-  Route.resource('contas', 'ContaController')
-    .apiOnly()
-    .validator(new Map([[['contas.store', 'contas.update'], ['Conta']]]))
-  Route.resource('fornecedores', 'FornecedorController')
-    .apiOnly()
-    .validator(
-      new Map([[['fornecedores.store', 'fornecedores.update'], ['Fornecedor']]])
-    )
-  Route.resource('nfs', 'NfController')
-    .apiOnly()
-    .validator(new Map([[['nfs.store', 'nfs.update'], ['Nf']]]))
+  Route.resource('teams', 'TeamController').apiOnly()
 
   Route.post('files', 'FileController.store')
 }).middleware('auth')
