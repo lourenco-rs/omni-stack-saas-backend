3c9057c0e493ea22c31b7ad7aae17d96fd2e6ade
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-05-11 08:16:59 -0300

    Configurações para deploy no heroku

--- config/database.js
+++ config/database.js
@@ -6,6 +6,10 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+const Url = require('url-parse')
+
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -32,7 +36,9 @@ module.exports = {
   sqlite: {
     client: 'sqlite3',
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get('DB_DATABASE', 'development')}.sqlite`
+      )
     },
     useNullAsDefault: true,
     debug: Env.get('DB_DEBUG', false)
@@ -69,7 +75,6 @@ module.exports = {
   |
   | npm i --save pg
   |
-  */
   pg: {
     client: 'pg',
     connection: {
@@ -80,5 +85,16 @@ module.exports = {
       database: Env.get('DB_DATABASE', 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
+  }
+  */
+  pg: {
+    client: 'pg',
+    connection: {
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+    }
   }
 }
